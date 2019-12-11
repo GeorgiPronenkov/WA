1a954c82b2c1c3a96a356009643fd0aaaab9b180
Georgi Pronenkov <45390436+J0RR0@users.noreply.github.com>
2019-12-11 23:25:36 +0200

    Update Vowels Count.js

--- Vowels Count.js
+++ Vowels Count.js
@@ -4,7 +4,8 @@ function getCount(input) {
 	return input
 		.replace(/[^aeiou]/gi, '')
 		.length; //replace with nothing
-	// const matches = str.match(/[aeiou]/g);
-	//return matches ? matches.length : 0; // ternary operator, if no matches - null is returned
+	
+	const matches = str.match(/[aeiou]/g);
+	return matches ? matches.length : 0; // ternary operator, if no matches - null is returned
 }
 console.log(getCount('abracadabra'));
