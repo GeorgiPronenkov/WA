e48cc572db812ca25b0868d11f40b0dee96d33fc
Georgi Pronenkov <45390436+J0RR0@users.noreply.github.com>
2019-12-13 18:42:27 +0200

    Add files via upload

---
+++ Sort odd and even numbers.js
@@ -0,0 +1,7 @@
+function sort(arr) {
+	let sorted =  arr.sort(function(a,b) {
+	  	return (a & 1) - (b & 1) || a-b;
+	});
+  console.log(sorted);
+}
+sort([10, 21, 4, 15, 7, 99, 0, 12]); //[ 0, 4, 10, 12, 7, 15, 21, 99 ]
---
+++ Strings sort in array.js
@@ -0,0 +1,9 @@
+function sort(arr) {
+	let defaultSort = arr.sort((a,b) => {
+	  	return a.toString().localeCompare(b);
+	});
+  console.log(defaultSort);
+}
+
+sort(['s', 't', 'a', 'c', 'K', 'o', 'v', 'E', 'r', 'f', 'l', 'W', '2', '1']);
+//['1', '2', 'a', 'c', 'E', 'f', 'K', 'l', 'o', 'r', 's', 't', 'v', 'W']
