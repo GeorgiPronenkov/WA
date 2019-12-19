5eb92c05020935678eeb0283b6a9ff385f4c5977
Georgi Pronenkov <45390436+J0RR0@users.noreply.github.com>
2019-12-20 00:12:15 +0200

    Update Remove String spaces.js

--- Remove String spaces.js
+++ Remove String spaces.js
@@ -1,5 +1,7 @@
 function noSpaces(input) {
 	return input.replace(/^\s+|\s+$/g, '');
-	//const noSpace = x => x.replace(/ /g, '');
+	
+	//2
+	const noSpace = x => x.replace(/ /g, '');
 }
 console.log(noSpaces('    8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'));
