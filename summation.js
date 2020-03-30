824c7b5c20dcedcd37c57f69d71ca975ad5c8015
Georgi Pronenkov <45390436+J0RR0@users.noreply.github.com>
2020-03-31 02:54:52 +0300

    Update summation.js

--- summation.js
+++ summation.js
@@ -10,3 +10,17 @@ function sum(number) {
 
 }
 console.log(sum(8));  //summation(8) => 36
+
+
+function solve(number) {
+    let numAsString = number.toString();
+    let sum = 0;
+
+    for (let i = 0; i < numAsString.length; i++) {
+        let digit = Number(numAsString.charAt(i));
+        sum += digit;
+    }
+    console.log(sum);
+ }
+
+ solve(245678); //32
