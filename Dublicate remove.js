93e1dabad4140afe1dbe1e10cbc9d30e93679688
Georgi Pronenkov <45390436+Pronenkov@users.noreply.github.com>
2020-04-06 03:21:16 +0300

    init

--- Dublicate remove.js
+++ Dublicate remove.js
@@ -4,3 +4,19 @@ function dublicate(input) {
 	return ([...new Set(resultInput)].join(' '));
 }
 console.log(dublicate('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
+
+function dublicate(arrNumbers) {
+	let sortArr = arrNumbers.sort();
+	// let newArr = [];
+	// for (let i = 0; i < arrNumbers.length; i++) {
+	// 	if (arrNumbers[i] === arrNumbers[i + 1]) {
+	// 	    newArr.push(arrNumbers[i]);
+	// 	}
+	// }
+	//return newArr;
+	
+	//2nd variant
+	let uniq = [...new Set(sortArr)];
+	return uniq;
+}
+console.log(dublicate([1,3,2,5,4,5,7,6,7])); //5
--- Find word in Array.js
+++ Find word in Array.js
@@ -9,3 +9,22 @@ function wordSearch(wordFind, text) {
 }
 
 console.log(wordSearch('the', 'what makes the desert beautiful, said the little prince is that somewhere it hides a well'));
+
+//with regEx
+function wordSearch(word, text) {
+	//1:
+	return new RegExp('\\b' + word + '\\b').test(text);
+	
+	//let texts = text.split();
+	//2:
+	let result = text.match(/\b\w+\b/g);
+	for (let i = 0; i < result.length; i++) {
+		let findWord = result[i];
+		if (word === findWord) {
+		    return true;
+		}
+	}
+	return  false;
+}
+
+console.log(wordSearch('prince', 'what makes the desert beautiful, said the little prince is that somewhere it hides a well'));
